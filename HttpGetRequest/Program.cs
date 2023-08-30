1c24fccab2bce7da9499a201a3caec8f54817f97
MNIGARR <nigarrmustafazada@gmail.com>
2023-08-30 14:44:18 +0400

    Add project files.

---
+++ HttpGetRequest/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Net;
+
+#region HttpPost
+//HttpClient client = new();
+//Post post = new()
+//{
+//    Id = 1,
+//    UserId = 1,
+//    Title = "Test",
+//    Body = "Test",
+//};
+
+//Uri uri = new Uri(@"https://jsonplaceholder.typicode.com/posts");
+//string json = JsonSerializer.Serialize(post);
+
+//StringContent stringContent= new StringContent(json);
+//HttpResponseMessage httpResponse = await client.PostAsync(uri, stringContent);
+
+//Console.WriteLine((int)httpResponse.StatusCode);
+//var jsonResponse = await httpResponse.Content.ReadAsStringAsync();
+
+//Console.WriteLine(jsonResponse);
+#endregion
+
+
+#region HttpDelete
+//HttpClient client = new();
+//Post post = new()
+//{
+//    UserId = 1,
+//    Id = 1,
+//    Title = "Test",
+//    Body = "Test",
+//};
+
+//Uri uri = new Uri(@"https://jsonplaceholder.typicode.com/posts/1");
+//string json = JsonSerializer.Serialize(post);
+
+//StringContent stringContent = new(json);
+
+//HttpResponseMessage httpResponse = await client.DeleteAsync(uri);
+//Console.WriteLine((int)httpResponse.StatusCode);
+
+//var jsonResponse = await httpResponse.Content.ReadAsStringAsync();
+//Console.WriteLine(jsonResponse);
+#endregion
+
+#region HttpPut
+//HttpClient client = new();
+//Post post = new()
+//{
+//    UserId = 1,
+//    Id = 1,
+//    Title = "Test",
+//    Body = "Test",
+//};
+
+//Uri uri = new Uri(@"https://jsonplaceholder.typicode.com/posts/1");
+//string json = JsonSerializer.Serialize(post);
+
+//StringContent stringContent = new(json);
+//HttpResponseMessage httpResponse = await client.PutAsync(uri, stringContent);
+
+//Console.WriteLine((int)httpResponse.StatusCode);
+//var jsonResponse = await httpResponse.Content.ReadAsStringAsync();
+
+//Console.WriteLine(jsonResponse);
+#endregion
